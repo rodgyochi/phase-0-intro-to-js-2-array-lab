bdc761ec1c2e433981f251c9587f54036969f5e9
Rodgers Opiyo <opiyorodgers030@gmail.com>
2023-11-05 12:09:23 +0300

    Completed Lab

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Write your solution here!
+const cats = ["Milo","Otis","Garfield"];
+function destructivelyAppendCat(name){
+    cats.push("Ralph");
+}
+function destructivelyPrependCat(name){
+    cats.unshift("Bob");
+}
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+function appendCat(name){
+  const newArray =[...cats,"Broom"];  
+  return newArray;
+} 
+function prependCat(name){
+    const newArray =["Arnold",...cats];
+    return newArray;
+}
+function removeLastCat(){
+    const cats = ["Milo","Otis","Garfield"];
+       const newArray =cats.splice(0,cats.length-1);
+       return newArray;
+}
+function removeFirstCat(){
+    const cats = ["Milo","Otis","Garfield"];
+    const newArray = cats.splice(1)
+    return newArray;
+}
